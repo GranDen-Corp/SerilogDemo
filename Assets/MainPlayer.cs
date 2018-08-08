1c59be72ae3232ddba2d8f85859de3de2998833f
Chen Yu Pao <windperson@gmail.com>
2018-08-08 11:16:49 +0800

    A demo using serilog logger to write to Unity3d Console

---
+++ Assets/MainPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SerilogUnity3dSink;
+using UnityEngine;
+using Logger = Serilog.Core.Logger;
+
+public class MainPlayer : MonoBehaviour {
+	private Logger _logger;
+
+	public MainPlayer()
+	{
+		_logger = new Serilog.LoggerConfiguration().WriteTo.UnityConsole().CreateLogger();
+	}
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Start MainPlayer");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_logger.Information("call update");
+	}
+}
